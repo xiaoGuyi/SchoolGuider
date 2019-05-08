51168c74b99ef00a3b95eaede41f7121cc9d9921
B17041623徐国玮 <535518917@qq.com>
2019-05-08 20:39:14 +0800

    sql文件

---
+++ Japanese.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- 主机： 127.0.0.1
+-- 生成日期： 2019-04-27 09:17:27
+-- 服务器版本： 10.1.37-MariaDB
+-- PHP 版本： 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 数据库： `guider`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `scenic`
+--
+
+CREATE TABLE `scenery_ja` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `imageNameList` varchar(255) DEFAULT NULL,
+  `introcude` varchar(255) DEFAULT NULL,
+  `voiceName` varchar(255) DEFAULT NULL,
+  `scenicName` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `scenic`
+--
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `scenic`
+--
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `scenic`
+--
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
